5f11bc1737fd31347189c2ed60d1262afd934741
JordJ19 <jordie.juwono96@gmail.com>
2023-01-08 12:22:24 +0700

    add bottom nav and finish home fragment

--- app/src/main/java/com/jordju/foodieapp/MainActivity.kt
+++ app/src/main/java/com/jordju/foodieapp/MainActivity.kt
@@ -17,5 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }
